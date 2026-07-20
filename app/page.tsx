4ed9978e19fb6c533a3db7841efa41938a7e9a62
RootGaurav <gauravrawat0259@gmail.com>
2026-07-20 16:41:43 +0530

    cicd check

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="rounded-lg bg-white p-10 shadow-lg">
-
         <h1 className="text-4xl font-bold text-blue-600">
-          {process.env.NEXT_PUBLIC_APP_NAME}
+          Vercel CI/CD Demo 🚀
         </h1>
 
-        <p className="mt-4 font-bold text-blue-600">
+        <p className="mt-4">
           Environment :
-          {" "}
           {process.env.NEXT_PUBLIC_ENVIRONMENT}
         </p>
-
       </div>
     </main>
   );
