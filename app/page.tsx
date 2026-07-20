a737b5f4f8288742c1dbdb561ee0bef9a81fd5f7
RootGaurav <gauravrawat0259@gmail.com>
2026-07-20 19:09:59 +0530

    Added Supabase integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-export default function Home() {
+import { supabase } from "@/lib/supabase";
+
+export default async function Home() {
+
+  const { data, error } = await supabase
+    .from("users")
+    .select("*");
+
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="rounded-lg bg-white p-10 shadow-lg">
-
         <h1 className="text-4xl font-bold text-blue-600">
-          {process.env.NEXT_PUBLIC_APP_NAME}
+          Vercel CI/CD Demo 🚀
         </h1>
 
-        <p className="mt-4 font-bold text-blue-600">
+        <p className="mt-4">
           Environment :
-          {" "}
           {process.env.NEXT_PUBLIC_ENVIRONMENT}
 
           
@@ -17,7 +22,6 @@ export default function Home() {
         <p className="mt-2">
          Feature branch changes.
         </p>
-
       </div>
     </main>
   );
