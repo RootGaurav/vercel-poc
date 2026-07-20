d5740812adde7d4c1b816f253cd9926861b7ab19
RootGaurav <gauravrawat0259@gmail.com>
2026-07-20 17:16:20 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,21 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="rounded-lg bg-white p-10 shadow-lg">
+
         <h1 className="text-4xl font-bold text-blue-600">
-          Vercel CI/CD Demo 🚀
+          {process.env.NEXT_PUBLIC_APP_NAME}
         </h1>
 
-        <p className="mt-4">
+        <p className="mt-4 font-bold text-blue-600">
           Environment :
+          {" "}
           {process.env.NEXT_PUBLIC_ENVIRONMENT}
+
+
+
+          Changes are implemented in dev branch 
         </p>
+
       </div>
     </main>
   );
